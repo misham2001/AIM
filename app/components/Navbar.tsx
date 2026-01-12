794c6ebd35446d169b6249f2adb9459b103ea41d
misham2001 <misham.d24@gmail.com>
2026-01-12 21:33:36 +0530

    fixes the build error

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { Menu, X, Church } from 'lucide-react';
+import { Menu, X } from 'lucide-react';
 import Image from 'next/image';
 
 export function Navbar() {
